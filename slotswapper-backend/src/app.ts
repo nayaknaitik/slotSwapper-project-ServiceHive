2b7a61452c093b897a310a2cccafb1c5aa864f09
Naitik Nayak <naitiknayak009@gmail.com>
2025-11-05 11:42:52 +0530

    addedFrontendUrltoBackend

--- slotswapper-backend/src/app.ts
+++ slotswapper-backend/src/app.ts
@@ -6,7 +6,8 @@ import swapRoutes from './routes/swapRoutes';
 
 const app = express();
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: ['http://localhost:3000',
+  'https://slot-swapper-project-service-hive.vercel.app/'],
   credentials: true,
   methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
